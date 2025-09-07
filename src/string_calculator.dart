371189e60d0cc0f50031a9a114c5242f01efc6ff
sumit <sumitmishraecosoul@gmail.com>
2025-09-07 13:16:19 +0530

    impl: return 0 when input is empty

--- src/string_calculator.dart
+++ src/string_calculator.dart
@@ -1,24 +1,8 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.isEmpty) return 0;
-    
-    var delimiter = RegExp(r'[,\n]');
-    var input = numbers;
-    
-    if (numbers.startsWith('//')) {
-      var lines = numbers.split('\n');
-      delimiter = RegExp(RegExp.escape(lines[0].substring(2)));
-      input = lines[1];
+    if (numbers.isEmpty) {
+      return 0;
     }
-    
-    var parts = input.split(delimiter);
-    var numbers_list = parts.map(int.parse).toList();
-    
-    var negatives = numbers_list.where((n) => n < 0).toList();
-    if (negatives.isNotEmpty) {
-      throw ArgumentError('negative numbers not allowed ${negatives.join(',')}');
-    }
-    
-    return numbers_list.reduce((a, b) => a + b);
+    throw UnimplementedError('Not implemented yet');
   }
 }
