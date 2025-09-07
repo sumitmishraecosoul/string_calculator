c07e273c02031b5be2a4f0d10a9d7707567b9444
sumit <sumitmishraecosoul@gmail.com>
2025-09-07 13:23:59 +0530

    refactor: extract helper methods and clean up duplication

--- src/string_calculator.dart
+++ src/string_calculator.dart
@@ -1,26 +1,37 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.isEmpty) {
-      return 0;
-    }
+    if (numbers.isEmpty) return 0;
     
-    var delimiter = RegExp(r'[,\n]');
-    var input = numbers;
-    
-    if (numbers.startsWith('//')) {
-      var lines = numbers.split('\n');
-      delimiter = RegExp(RegExp.escape(lines[0].substring(2)));
-      input = lines[1];
-    }
+    var delimiter = _getDelimiter(numbers);
+    var input = _getInput(numbers);
     
     var parts = input.split(delimiter);
     var numbers_list = parts.map(int.parse).toList();
     
-    var negatives = numbers_list.where((n) => n < 0).toList();
+    _validateNoNegatives(numbers_list);
+    
+    return numbers_list.reduce((a, b) => a + b);
+  }
+  
+  RegExp _getDelimiter(String numbers) {
+    if (numbers.startsWith('//')) {
+      var lines = numbers.split('\n');
+      return RegExp(RegExp.escape(lines[0].substring(2)));
+    }
+    return RegExp(r'[,\n]');
+  }
+  
+  String _getInput(String numbers) {
+    if (numbers.startsWith('//')) {
+      return numbers.split('\n')[1];
+    }
+    return numbers;
+  }
+  
+  void _validateNoNegatives(List<int> numbers) {
+    var negatives = numbers.where((n) => n < 0).toList();
     if (negatives.isNotEmpty) {
       throw ArgumentError('negative numbers not allowed ${negatives.join(',')}');
     }
-    
-    return numbers_list.reduce((a, b) => a + b);
   }
 }
