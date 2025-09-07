5d6a67674b7ab46b85c32ecf936f16768ce69bf1
sumit <sumitmishraecosoul@gmail.com>
2025-09-07 13:18:32 +0530

    test: add failing test for two comma-separated numbers

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -16,5 +16,9 @@ void main() {
     test('should return the number for single number', () {
       expect(calculator.add('1'), equals(1));
     });
+
+    test('should add two comma-separated numbers', () {
+      expect(calculator.add('1,5'), equals(6));
+    });
   });
 }
